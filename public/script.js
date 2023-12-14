86057748e2748e442ddb57c1083aebd4eee14d7d
Gabriel Felix <g.fsilva.ext@picpay.com>
2023-12-14 11:44:46 -0300

    second commit

--- public/script.js
+++ public/script.js
@@ -1,3 +1,13 @@
 /* eslint-disable no-undef */
 // eslint-disable-next-line no-unused-vars
 const socket = io("http://localhost:8000");
+
+// socket.on("connect", () => {
+// 	console.log("achou");
+// });
+
+const txt = document.getElementById("editor-texto");
+
+txt.addEventListener("keyup", () => {
+	socket.emit("txt-writing", txt.value);
+});
